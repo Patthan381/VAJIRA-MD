010fdc1857d273af6340f4cc95b34e10eacab36d
Patthan381 <patthan381@outlook.com>
2024-08-02 22:20:05 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,10 +11,10 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=WcMU2YiT#6s419BscqPJwZ-Uts0qB3ymZJPyZZe8b4-ueiqdmllo' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=ycdx0DCa#t5haN7pQRmZEgzxiJsntQeUbWFPMEzQlb8CdNrSCU9Y' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
-MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
+MAX_SIZE: process.env.MAX_SIZE === undefined ? '10536': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
